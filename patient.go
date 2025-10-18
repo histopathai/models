0ea95479ea976a499929bc98f6838e9a25244777
yasintuncerr <yasintuncerr@gmail.com>
2025-10-18 21:15:57 +0300

    Add History field to Patient model for enhanced patient data tracking

--- patient.go
+++ patient.go
@@ -4,12 +4,12 @@ package models
 import "time"
 
 type Patient struct {
-	ID      string  `firestore:"id,omitempty"` // Firestore ID veya kendi ID'niz
-	Age     *int    `firestore:"age,omitempty"`
-	Gender  *string `firestore:"gender,omitempty"`
-	Race    *string `firestore:"race,omitempty"`
-	Disease *string `firestore:"disease,omitempty"`
-
+	ID        string    `firestore:"id,omitempty"` // Firestore ID veya kendi ID'niz
+	Age       *int      `firestore:"age,omitempty"`
+	Gender    *string   `firestore:"gender,omitempty"`
+	Race      *string   `firestore:"race,omitempty"`
+	Disease   *string   `firestore:"disease,omitempty"`
+	History   *string   `firestore:"history,omitempty"`
 	CreatedAt time.Time `firestore:"created_at"`
 	UpdatedAt time.Time `firestore:"updated_at"`
 }
