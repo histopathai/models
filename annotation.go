fe61f24886bdc2b835d557af197669a29090ba78
yasintuncerr <yasintuncerr@gmail.com>
2025-10-18 20:20:34 +0300

    Refactor Annotation model to streamline fields

--- annotation.go
+++ annotation.go
@@ -4,41 +4,27 @@ import (
 	"time"
 )
 
-type LabelType string
-type LabelValue string
-type Classes map[string]float64
-type RegressionRange [2]float64
-
-const (
-	ClassificationLabel LabelType = "classification"
-	RegressionLabel     LabelType = "regression"
-)
-
-type Annotation struct {
-	ID         string          `json:"id"`
-	ImageID    string          `json:"image_id"` // ID of the associated image
-	UserID     string          `json:"user_id"`  // ID of the user who created the annotation
-	Polygons   string          `json:"polygons"` // Serialized polygon data (e.g., GeoJSON)
-	LabelType  LabelType       `json:"label_type,omitempty"`
-	LabelValue LabelValue      `json:"label_value,omitempty"`
-	Classes    Classes         `json:"classes,omitempty"`
-	Range      RegressionRange `json:"range,omitempty"`
-
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+type AnnotationType struct {
+	ID                   string     `firestore:"id"`
+	Name                 string     `firestore:"name"`
+	Desc                 string     `firestore:"desc"`
+	ScoreEnable          bool       `firestore:"score_enable"`
+	ScoreName            string     `firestore:"score_name"`
+	ScoreRange           [2]float64 `firestore:"score_range"`
+	ClassificaitonEnable bool       `firestore:"classification_enable"`
+	ClassList            []string   `firestore:"class_list"`
+	CreatedAt            time.Time  `firestore:"created_at"`
+	UpdatedAt            time.Time  `firestore:"updated_at"`
 }
 
-func (ann *Annotation) ToJSON() map[string]interface{} {
-	return map[string]interface{}{
-		"id":          ann.ID,
-		"image_id":    ann.ImageID,
-		"user_id":     ann.UserID,
-		"polygons":    ann.Polygons,
-		"label_type":  ann.LabelType,
-		"label_value": ann.LabelValue,
-		"classes":     ann.Classes,
-		"range":       ann.Range,
-		"created_at":  ann.CreatedAt,
-		"updated_at":  ann.UpdatedAt,
-	}
+type Annotation struct {
+	ID        string    `firestore:"id"`
+	ImageID   string    `firestore:"image_id"`        // ID of the associated image
+	CreatorID string    `firestore:"creator_id"`      // ID of the user who created the annotation
+	TypeID    string    `firestore:"type_id"`         // ID of the annotation type
+	Polygon   string    `firestore:"polygon"`         // Serialized polygon data (e.g., GeoJSON)
+	Class     string    `firestore:"class,omitempty"` // Class label for the annotation
+	Score     float64   `firestore:"score,omitempty"` // Score value for the annotation
+	CreatedAt time.Time `firestore:"created_at"`
+	UpdatedAt time.Time `firestore:"updated_at"`
 }
