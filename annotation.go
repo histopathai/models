dabfc953272a5a6d3fcc5fde45525ff09ea97067
yasintuncerr <yasintuncerr@gmail.com>
2025-10-18 21:15:48 +0300

    Refactor Annotation model to change Polygon field from a single Point to a slice of Points for better polygon representation

--- annotation.go
+++ annotation.go
@@ -27,7 +27,7 @@ type Annotation struct {
 	ImageID   string    `firestore:"image_id"`        // ID of the associated image
 	CreatorID string    `firestore:"creator_id"`      // ID of the user who created the annotation
 	TypeID    string    `firestore:"type_id"`         // ID of the annotation type
-	Polygon   Point     `firestore:"polygon"`         // Serialized polygon data (e.g., GeoJSON)
+	Polygon   []Point   `firestore:"polygon"`         // Serialized polygon data (e.g., GeoJSON)
 	Class     *string   `firestore:"class,omitempty"` // Class label for the annotation
 	Score     *float64  `firestore:"score,omitempty"` // Score value for the annotation
 	CreatedAt time.Time `firestore:"created_at"`
