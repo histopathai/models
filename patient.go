f431a54883b099c89b84914ef126d1c0d28edf1a
yasintuncerr <yasintuncerr@gmail.com>
2025-10-18 21:09:44 +0300

    Add Patient model with fields for ID, Age, Gender, Race, Disease, CreatedAt, and UpdatedAt

---
+++ patient.go
@@ -0,0 +1,15 @@
+// models/patient.go (Yeni dosya)
+package models
+
+import "time"
+
+type Patient struct {
+	ID      string  `firestore:"id,omitempty"` // Firestore ID veya kendi ID'niz
+	Age     *int    `firestore:"age,omitempty"`
+	Gender  *string `firestore:"gender,omitempty"`
+	Race    *string `firestore:"race,omitempty"`
+	Disease *string `firestore:"disease,omitempty"`
+
+	CreatedAt time.Time `firestore:"created_at"`
+	UpdatedAt time.Time `firestore:"updated_at"`
+}
