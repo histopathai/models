b939596038b9fd33c08bca0eda61e1295e770f03
yasintuncerr <yasintuncerr@gmail.com>
2025-10-18 20:44:10 +0300

    Refactor Annotation model to use Point type for polygon and update ID field to be omitempty

--- annotation.go
+++ annotation.go
@@ -4,8 +4,13 @@ import (
 	"time"
 )
 
+type Point struct {
+	X float64 `firestore:"x"`
+	Y float64 `firestore:"y"`
+}
+
 type AnnotationType struct {
-	ID                   string     `firestore:"id"`
+	ID                   string     `firestore:"id,omitempty"`
 	Name                 string     `firestore:"name"`
 	Desc                 string     `firestore:"desc"`
 	ScoreEnable          bool       `firestore:"score_enable"`
@@ -18,11 +23,11 @@ type AnnotationType struct {
 }
 
 type Annotation struct {
-	ID        string    `firestore:"id"`
+	ID        string    `firestore:"id,omitempty"`
 	ImageID   string    `firestore:"image_id"`        // ID of the associated image
 	CreatorID string    `firestore:"creator_id"`      // ID of the user who created the annotation
 	TypeID    string    `firestore:"type_id"`         // ID of the annotation type
-	Polygon   string    `firestore:"polygon"`         // Serialized polygon data (e.g., GeoJSON)
+	Polygon   Point     `firestore:"polygon"`         // Serialized polygon data (e.g., GeoJSON)
 	Class     string    `firestore:"class,omitempty"` // Class label for the annotation
 	Score     float64   `firestore:"score,omitempty"` // Score value for the annotation
 	CreatedAt time.Time `firestore:"created_at"`
