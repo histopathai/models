6a1e33c87694f05b3fe6c27b53c6a2d8fc29c0fc
yasintuncerr <yasintuncerr@gmail.com>
2025-10-21 20:59:31 +0300

    Add AnnotationTypeID field to Workspace model and update ToMap/FromMap methods for Firestore integration

--- workspace.go
+++ workspace.go
@@ -5,35 +5,37 @@ import (
 )
 
 type Workspace struct {
-	ID             string    `firestore:"id,omitempty"`
-	Name           string    `firestore:"name"`
-	OrganType      string    `firestore:"organ_type"`
-	CreatorID      string    `firestore:"creator_id"` // User ID of the creator
-	Description    string    `firestore:"description"`
-	License        string    `firestore:"license"`
-	Organization   string    `firestore:"organization"`
-	ResourceURL    string    `firestore:"resource_url"`
-	ReleaseYear    int       `firestore:"release_year,omitempty"`
-	ReleaseVersion string    `firestore:"release_version,omitempty"`
-	CreatedAt      time.Time `firestore:"created_at"`
-	UpdatedAt      time.Time `firestore:"updated_at"`
+	ID               string    `firestore:"id,omitempty"`
+	Name             string    `firestore:"name"`
+	OrganType        string    `firestore:"organ_type"`
+	CreatorID        string    `firestore:"creator_id"` // User ID of the creator
+	Description      string    `firestore:"description"`
+	License          string    `firestore:"license"`
+	Organization     string    `firestore:"organization"`
+	ResourceURL      string    `firestore:"resource_url"`
+	ReleaseYear      int       `firestore:"release_year,omitempty"`
+	ReleaseVersion   string    `firestore:"release_version,omitempty"`
+	AnnotationTypeID string    `firestore:"annotation_type_id,omitempty"`
+	CreatedAt        time.Time `firestore:"created_at"`
+	UpdatedAt        time.Time `firestore:"updated_at"`
 }
 
 // ToMap converts Workspace to map for Firestore
 func (w *Workspace) ToMap() map[string]interface{} {
 	return map[string]interface{}{
-		"id":              w.ID,
-		"name":            w.Name,
-		"organ_type":      w.OrganType,
-		"creator_id":      w.CreatorID,
-		"description":     w.Description,
-		"license":         w.License,
-		"organization":    w.Organization,
-		"resource_url":    w.ResourceURL,
-		"release_year":    w.ReleaseYear,
-		"release_version": w.ReleaseVersion,
-		"created_at":      w.CreatedAt,
-		"updated_at":      w.UpdatedAt,
+		"id":                 w.ID,
+		"name":               w.Name,
+		"organ_type":         w.OrganType,
+		"creator_id":         w.CreatorID,
+		"description":        w.Description,
+		"license":            w.License,
+		"organization":       w.Organization,
+		"resource_url":       w.ResourceURL,
+		"release_year":       w.ReleaseYear,
+		"release_version":    w.ReleaseVersion,
+		"annotation_type_id": w.AnnotationTypeID,
+		"created_at":         w.CreatedAt,
+		"updated_at":         w.UpdatedAt,
 	}
 }
 
@@ -62,6 +64,9 @@ func (w *Workspace) FromMap(data map[string]interface{}) {
 	}
 	if v, ok := data["resource_url"].(string); ok {
 		w.ResourceURL = v
+	}
+	if v, ok := data["annotation_type_id"].(string); ok {
+		w.AnnotationTypeID = v
 	}
 	if v, ok := data["release_year"].(int64); ok {
 		w.ReleaseYear = int(v)
